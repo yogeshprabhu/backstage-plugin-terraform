e580db8af61cd8188f0809eea7ffdcd18119a01b
yogeshprabhu <yogesh.prabhu@gmail.com>
2023-07-25 10:22:03 -0400

    updating the main.tf to create an ec2 instance

--- examples/terraform/content/main.tf
+++ examples/terraform/content/main.tf
@@ -1,26 +1,87 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    random = {
+      source  = "hashicorp/random"
+    }
+  }
+  required_version = ">= 1.1.0"
 
+  cloud {
+    organization = "YoHashi"
 
-variable "second_hello" {
-  type = string
-  description = "Second entity to greet"
+    workspaces {
+      name = "aws-ec2-instance"
+    }
+  }
 }
 
-variable "secret_key" {
-  type = string
-  description = "A secret key to pass to some_key"
-  sensitive = true
+provider "aws" {
+  region = "us-west-2"
 }
 
-module "infrastructure" {
-  source  = "joatmon08/hello/random"
-  version = "6.0.0"
+resource "random_pet" "sg" {}
 
-  hellos = {
-    hello = "${{ values.name }}"
-    second_hello = var.second_hello
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-  some_key = var.secret_key
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name = "aws-yoge"
+  
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
 }
